12b07aa9e473769e77f21d8393dc82c3005c619f
yucihent <yucihent@163.com>
2023-09-04 21:41:17 +0800

    feat(example): :sparkles: 添加example展示各util

---
+++ example/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Debounce from './components/Debounce'
+</script>
+
+<template>
+  <section class="min-h-screen p-5">
+    <header class="text-center">
+      <h3 class="text-2xl mb-10">@yuci/utils</h3>
+    </header>
+
+    <main>
+      <Debounce />
+    </main>
+  </section>
+</template>
---
+++ example/src/components/Debounce.tsx
@@ -0,0 +1,3 @@
+export default function Debounce() {
+  return <div>Debounce</div>
+}
