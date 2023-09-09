7e5a53c38667b7899e5e701c360867f51654cccc
yucihent <yucihent@163.com>
2023-09-09 21:23:04 +0800

    feat(example): :sparkles: example中新增Debounce

--- example/src/components/Debounce.tsx
+++ example/src/components/Debounce.tsx
@@ -1,3 +1,34 @@
-export default function Debounce() {
-  return <div>Debounce</div>
-}
+import { defineComponent, ref } from 'vue'
+import { debounce } from '@yuci/utils'
+
+export default defineComponent({
+  name: 'Debounce',
+  setup() {
+    const count = ref(0)
+
+    // 默认延迟300ms
+    const onClick = debounce(() => {
+      count.value++
+    })
+
+    // 默认1000ms
+    const onClick2 = debounce(() => {
+      count.value++
+    }, 1000)
+
+    return () => (
+      <>
+        <h3 class="text-lg mb-3">{count.value}</h3>
+        <a-space>
+          <a-button type="primary" onClick={onClick}>
+            300ms Delay
+          </a-button>
+
+          <a-button type="primary" onClick={onClick2}>
+            1s Delay
+          </a-button>
+        </a-space>
+      </>
+    )
+  }
+})
